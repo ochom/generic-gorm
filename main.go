643314d7f871ff51fd973cf09a6c352e0297aba6
ochom <ochomrichard752@gmail.com>
2023-01-20 11:46:52 +0300

    feat: get connection

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package grm
 
-import "gorm.io/gorm/logger"
+import (
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
 
 // this will be initialized by the init function and reused across library
 var conn *Connection
@@ -11,4 +14,14 @@ func Init(platform Platform, dsn string, logLevel logger.LogLevel) *Connection {
 		conn = newConnection(platform, dsn, logLevel)
 	}
 	return conn
+}
+
+// Migrate ...
+func Migrate(models ...interface{}) error {
+	return conn.DB.AutoMigrate(models...)
+}
+
+// GetConnection get the connection
+func GetConnection() *gorm.DB {
+	return conn.DB
 }
